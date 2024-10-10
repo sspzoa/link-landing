1930b13dbe370180abafa3bc7f564fcc3f6f1ba4
Seungpyo Suh <me@sspzoa.io>
2024-10-11 08:44:31 +0900

    feat: add sponsors

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import { useIntersection } from '@/hooks/useIntersection';
+import Image from 'next/image';
 
 export default function Information() {
   const refs = [
+    useIntersection<HTMLDivElement>(),
+    useIntersection<HTMLDivElement>(),
+    useIntersection<HTMLDivElement>(),
+    useIntersection<HTMLDivElement>(),
     useIntersection<HTMLDivElement>(),
     useIntersection<HTMLDivElement>(),
     useIntersection<HTMLDivElement>(),
@@ -28,56 +33,71 @@ export default function Information() {
       </div>
       <table>
         <tbody>
-          <tr>
-            <td className="pb-[30px] pr-[30px] md:pr-[60px] align-top">
+        <tr>
+          <td className="pb-[30px] pr-[30px] md:pr-[60px] align-top">
               <span ref={refs[2]} className="text-gray500 text-paragraph text-nowrap">
                 행사명
               </span>
-            </td>
-            <td className="align-top">
-              <span ref={refs[6]} className="text-gray1000 text-paragraph">
+          </td>
+          <td className="align-top">
+              <span ref={refs[3]} className="text-gray1000 text-paragraph">
                 <strong>Link Demo Day</strong>
               </span>
-            </td>
-          </tr>
-          <tr>
-            <td className="pb-[30px] pr-[30px] md:pr-[60px] align-top">
-              <span ref={refs[3]} className="text-gray500 text-paragraph text-nowrap">
+          </td>
+        </tr>
+        <tr>
+          <td className="pb-[30px] pr-[30px] md:pr-[60px] align-top">
+              <span ref={refs[4]} className="text-gray500 text-paragraph text-nowrap">
                 행사 인원
               </span>
-            </td>
-            <td className="align-top">
-              <span ref={refs[7]} className="text-gray1000 text-paragraph">
+          </td>
+          <td className="align-top">
+              <span ref={refs[5]} className="text-gray1000 text-paragraph">
                 <strong>200명 내외</strong>
               </span>
-            </td>
-          </tr>
-          <tr>
-            <td className="pb-[30px] pr-[30px] md:pr-[60px] align-top">
-              <span ref={refs[4]} className="text-gray500 text-paragraph text-nowrap">
+          </td>
+        </tr>
+        <tr>
+          <td className="pb-[30px] pr-[30px] md:pr-[60px] align-top">
+              <span ref={refs[6]} className="text-gray500 text-paragraph text-nowrap">
                 행사 장소
               </span>
-            </td>
-            <td className="align-top">
-              <span ref={refs[8]} className="text-gray1000 text-paragraph">
+          </td>
+          <td className="align-top">
+              <span ref={refs[7]} className="text-gray1000 text-paragraph">
                 <strong>프론트원 (FRONT1) 5층 박병원홀 (서울특별시 마포구 마포대로 122)</strong>
               </span>
-            </td>
-          </tr>
-          <tr>
-            <td className="pb-[30px] pr-[30px] md:pr-[60px] align-top">
-              <span ref={refs[5]} className="text-gray500 text-paragraph text-nowrap">
+          </td>
+        </tr>
+        <tr>
+          <td className="pb-[30px] pr-[30px] md:pr-[60px] align-top">
+              <span ref={refs[8]} className="text-gray500 text-paragraph text-nowrap">
                 행사 일시
               </span>
-            </td>
-            <td className="align-top">
+          </td>
+          <td className="align-top">
               <span ref={refs[9]} className="text-gray1000 text-paragraph">
                 <strong>2024년 11월 16일 (토) 12:00 ~ 18:00</strong>
               </span>
-            </td>
-          </tr>
+          </td>
+        </tr>
         </tbody>
       </table>
+      <span ref={refs[10]} className="text-gray500 text-paragraph text-nowrap">
+        행사 후원
+      </span>
+      <div ref={refs[11]} className="flex flex-row w-full gap-8 justify-center items-center">
+        <img src="/sponsors/ansan.png" alt="ansan" width={256}/>
+        <img src="/sponsors/hanbit.png" alt="hanbit" width={256}/>
+        <img src="/sponsors/inflearn.png" alt="inflearn" width={256}/>
+      </div>
+      <span ref={refs[12]} className="text-gray500 text-paragraph text-nowrap">
+        장소 후원
+      </span>
+      <div ref={refs[13]} className="flex flex-row w-full gap-8 justify-center items-center">
+        <img src="/sponsors/dcamp.png" alt="ansan" width={256}/>
+        <img src="/sponsors/front1.png" alt="hanbit" width={256}/>
+      </div>
     </div>
   );
 }
