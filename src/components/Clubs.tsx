3c98816c49f7f233166278a2626c0e6aea171be3
Seungpyo Suh <me@sspzoa.io>
2024-10-23 20:06:45 +0900

    push again

--- src/components/Clubs.tsx
+++ src/components/Clubs.tsx
@@ -19,7 +19,8 @@ export default function Clubs() {
         <strong>
           한국디지털미디어고등학교, 단국대학교부속소프트웨어고등학교, 부산소프트웨어마이스터고등학교,
           <br className="hidden md:block" />
-          선린인터넷고등학교, 미림마이스터고등학교, 광주소프트웨어마이스터고등학교, 경북소프트웨어마이스터고등학교의 11개 동아리
+          선린인터넷고등학교, 미림마이스터고등학교, 광주소프트웨어마이스터고등학교, 경북소프트웨어마이스터고등학교의
+          11개 동아리
         </strong>
         와 함께합니다.
       </span>
@@ -36,18 +37,18 @@ export default function Clubs() {
       </div>
       <div ref={refs[2]} className="max-w-4xl flex flex-wrap justify-center items-center gap-8 md:gap-16">
         <img className="w-12 md:w-16" src="/club-logo/LUNA.svg" />
-        <img className="w-12 md:w-16" src="/club-logo/noid.svg"/>
-        <img className="w-12 md:w-16" src="/club-logo/mrmr.svg"/>
+        <img className="w-12 md:w-16" src="/club-logo/noid.png" />
+        <img className="w-12 md:w-16" src="/club-logo/mrmr.svg" />
 
-        <img className="w-12 md:w-16" src="/club-logo/crevice.svg"/>
-        <img className="w-12 md:w-16" src="/club-logo/msg.svg"/>
-        <img className="h-6 md:h-8" src="/club-logo/devcoop.svg"/>
+        <img className="w-12 md:w-16" src="/club-logo/crevice.svg" />
+        <img className="w-12 md:w-16" src="/club-logo/msg.svg" />
+        <img className="h-6 md:h-8" src="/club-logo/devcoop.svg" />
 
-        <img className="w-12 md:w-16" src="/club-logo/tapey.svg"/>
-        <img className="w-12 md:w-16" src="/club-logo/absolute.svg"/>
-        <img className="h-6 md:h-8" src="/club-logo/miven.svg"/>
-        <img className="w-12 md:w-16" src="/club-logo/implude.svg"/>
-        <img className="w-12 md:w-16" src="/club-logo/wine.svg"/>
+        <img className="w-12 md:w-16" src="/club-logo/tapey.svg" />
+        <img className="w-12 md:w-16" src="/club-logo/absolute.svg" />
+        <img className="h-6 md:h-8" src="/club-logo/miven.svg" />
+        <img className="w-12 md:w-16" src="/club-logo/implude.svg" />
+        <img className="w-12 md:w-16" src="/club-logo/wine.svg" />
       </div>
     </div>
   );
